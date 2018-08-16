76b2b7076a8e14baff3cba4a29453394bfeb0d70
Apizaco <jesus.rodrigu20@anahuac.mx>
2018-08-16 18:38:43 -0500

    apizaco

---
+++ platforms/browser/www/js/routes.js
@@ -0,0 +1,20 @@
+var routes = [
+    {
+      path: '/register/',
+      url: 'views/register.html',
+    },{
+      path: '/ola/',
+      url: 'views/ola.html',
+    },{
+       path: '/home/',
+      url: 'views/home.html',
+    },
+    {
+      path: '/login/',
+      url: 'index.html',
+    },
+    {
+       path: '/home/',
+      url: 'views/home.html',
+    }
+  ];
